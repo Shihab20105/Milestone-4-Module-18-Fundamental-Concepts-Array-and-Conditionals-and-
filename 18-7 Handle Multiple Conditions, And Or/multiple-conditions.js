5fc10aa27e95917aea06679cc171f7e66c250f4e
shihab20105 <shihabur10@gmail.com>
2024-03-21 23:33:28 +0000

    Discovered Handle Multiple Conditions, And Or

---
+++ 18-7 Handle Multiple Conditions, And Or/multiple-conditions.js
@@ -0,0 +1,50 @@
+// var isGraduated = true;
+// var salary = 25000;
+// var salary = 75000;
+// var cars = 0;
+// var cars = 1;
+
+
+// if(isGraduated === true){
+//     console.log('lets make a deal');
+// }
+// else{
+//     console.log('you can not able to get it');
+// }
+
+// if(isGraduated == true && salary > 50000 ){
+//     console.log('lets make a deal');
+// }
+// else{
+//     console.log('you can not able to get it');
+// }
+
+// if(isGraduated == true && salary > 50000 && cars >= 1){
+//     console.log('lets make a deal');
+// }
+// else{
+//     console.log('you can not able to get it');
+// }
+
+// var isGraduated = true;
+var isGraduated = false;
+// var salary = 5000;
+// var salary = 51000;
+// var salary = 31000;
+var salary = 61000;
+// var cars = 1;
+var cars = 0;
+
+if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('let us work together');
+}
+else{
+    console.log('i will contact to other person');
+}
+
+// if((isGraduated === true && salary > 50000) || cars >= 1){
+
+// }
+if(isGraduated === true && (salary > 50000 || cars >= 1)){
+
+}
