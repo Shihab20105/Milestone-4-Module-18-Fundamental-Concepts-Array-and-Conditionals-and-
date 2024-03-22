62a87d9b67601f43218df9cbd8d977cf45acf32c
shihab20105 <shihabur10@gmail.com>
2024-03-22 23:33:45 +0000

    Discovered (Advanced) Multi Stage Condition And Nested Conditions

---
+++ 18-8 Ad. Multi Stage Condition & Nested/multi-stage-conditions.js
@@ -0,0 +1,21 @@
+// var money = 100;
+// var money = 10;
+// var money = 30;
+// var money = 40;
+var money = 68;
+var butterCroissantPrice = 45;
+var butterBread = 35;
+var toastBiscuit = 20;
+
+if(butterCroissantPrice < money) {
+    console.log('then i will have butter croissant');
+}
+else if (butterBread < money) {
+    console.log('then i will have butter bread');
+}
+else if (toastBiscuit < money) {
+    console.log('then i will have toast biscuit');
+}
+else {
+    console.log('tea will be better option');
+}
---
+++ 18-8 Ad. Multi Stage Condition & Nested/nested-conditions.js
@@ -0,0 +1,21 @@
+var math = true;
+var geometry = true;
+var straightLine = false; 
+
+if(math == true){
+
+}
+else{
+    if(geometry == true){
+        if(straightLine == true){
+
+        }
+        else{
+            console.log('you got to maintain straight line');
+        }
+
+    }
+    else{
+        console.log('you can not be a pythagoras');
+    }
+}
