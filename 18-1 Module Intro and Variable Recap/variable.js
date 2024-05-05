caceb5f1de0f1cceb30a30679343dd10445a2c55
shihab20105 <shihabur10@gmail.com>
2024-05-05 19:07:02 +0100

    Explored Module Introduction And Variable Recap

--- 18-1 Module Intro and Variable Recap/variable.js
+++ 18-1 Module Intro and Variable Recap/variable.js
@@ -1,19 +1,23 @@
-// To declare a variable you need five things
+/*                      Variable Recap
 
-// number type variable
+To declare a variable you need five things */
+
+
+/* number type variable */
 var sunglassPrice = 120;
 
-// string type variable
+/* string type variable */
 var specialOneName = 'Jhose Morihioho';
 
-// boolean type variable
+/* boolean type variable */
 var isSingle = true;
 
-// two types of number: Integer: 120,
-// float, decimal: 3.65,
+/* two types of number:
+ 
+Integer: 120,
+float, decimal: 3.65 */
 
-// variable naming convention: rules, best practice
-/**
+/* variable naming convention: rules, best practice
  * 1. single word
  * 2. no gap or space
  * 3. no quote
@@ -23,11 +27,11 @@ var isSingle = true;
  * 7. should use camelCase
  */ 
 
-// Math Operations: +, -, *, /
-// Shorthand: +=, -=, ++, --
+/* Math Operations: +, -, *, /
+Shorthand: +=, -=, ++, --
 
-// convert: parseInt, parseFloat
+convert: parseInt, parseFloat
 
-// toFixed
+toFixed
 
-// remainder(vagshesh): %
+remainder(vagshesh): % */
