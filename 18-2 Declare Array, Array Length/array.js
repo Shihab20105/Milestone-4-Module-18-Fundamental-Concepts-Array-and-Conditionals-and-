7216729eb5fa89f08aa9d9201d3fc540fe3742fa
shihab20105 <shihabur10@gmail.com>
2024-05-23 20:22:53 +0100

    Explored Declare Array, Array Length And Array Index

--- 18-2 Declare Array, Array Length/array.js
+++ 18-2 Declare Array, Array Length/array.js
@@ -1,10 +1,22 @@
+/*                          Array */
+
 var friendsAge = [11, 21, 45, 17, 14, 105, 6];
 var picnicFee = [5000, 2000, 1000, 150];
 var flowers = ['Rose', 'Lily', 'Daisy', 'Lavender', 'Dahlia'];
 var vowels = ['a', 'e', 'i', 'o', 'u'];
 
-// console.log(vowels);
-// console.log(picnicFee);
-// console.log(picnicFee.length);
-// console.log(vowels.length);
+console.log(vowels);
+/* Output:
+[ 'a', 'e', 'i', 'o', 'u' ] */
+console.log(picnicFee);
+/* Output:
+[ 5000, 2000, 1000, 150 ] */
+console.log(picnicFee.length);
+/* Output:
+4 */
+console.log(vowels.length);
+/* Output:
+5 */
 console.log(friendsAge.length);
+/* Output:
+7 */
