b0748ae66b361df94e1dcd6c95ab87729de705c4
shihab20105 <shihabur10@gmail.com>
2024-05-24 01:06:14 +0100

    Discovered Array Index, Get And Set By Index, IndexOf

--- 18-3 Array Index, Get And Set Index/index.js
+++ 18-3 Array Index, Get And Set Index/index.js
@@ -1,24 +1,43 @@
+/*                          Index */
+
 var numbers = [45, 68, 78, 56, 89, 98];
 
-// Index ( [45, 68, 78, 56, 89, 98]; )
-//          0    1   2   3   4   5
+/* Index ( [45, 68, 78, 56, 89, 98]; )
+            0    1   2   3   4   5 */
 
-// 1. Get element value by index
-// console.log(numbers);
-// console.log(numbers[0]);
-// console.log(numbers[3]);
-// console.log(numbers[2]);
+
+/* 1. Get element value by index: */
+console.log(numbers);
+/* Output:
+[ 45, 68, 78, 56, 89, 98 ] */
+console.log(numbers[0]);
+/* Output:
+45 */
+console.log(numbers[3]);
+/* Output:
+56 */
+console.log(numbers[2]);
+/* Output:
+78 */
 
 var elements = numbers[1];
-// console.log(elements);
+console.log(elements);
+/* Output:
+68 */
 
-// 2. Set element value by index
+/* 2. Set element value by index: */
 numbers[1] = 77;
 numbers[3] = 44;
-// console.log(numbers);
+console.log(numbers);
+/* Output:
+[ 45, 77, 78, 44, 89, 98 ] */
 
-// 3. Find index of an element
-// var positionIndex = numbers.indexOf(89);
-// var positionIndex = numbers.indexOf(188);
+/* 3. Find index of an element: */
 var positionIndex = numbers.indexOf(89);
 console.log(positionIndex);
+/* Output:
+4 */
+var positionIndex = numbers.indexOf(188);
+console.log(positionIndex);
+/* Output:
+-1 */
