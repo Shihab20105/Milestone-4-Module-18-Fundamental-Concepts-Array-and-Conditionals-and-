9d1ec7332d216b6d70ee4d980df39f1d92674fd3
shihab20105 <shihabur10@gmail.com>
2024-05-27 20:32:31 +0100

    Discovered Add Or Remove Element From Array Using Push, Pop

--- 18-4 Add Or Remove Element Push, Pop/push-pop.js
+++ 18-4 Add Or Remove Element Push, Pop/push-pop.js
@@ -1,32 +1,52 @@
-var numbers = [78, 45, 98, 45];
+/*                          Add Or Remove Element */
 
-// Use push to add an element to an array as the last element of the array.
+
+/* Use push to add an element to an array as the last element of the array. */
+var numbers = [78, 45, 98, 45];
 numbers.push(63);
-// console.log(numbers);
+console.log(numbers);
+/* Output:
+[ 78, 45, 98, 45, 63 ] */
+
+
+/* Use pop to get rid of the last element. */
+numbers.pop();
+numbers.pop();
+console.log(numbers);
+/* Output: 
+[ 78, 45, 98 ] */
+
+
+var element = numbers.pop();
+console.log(numbers);
+/* Output: 
+[ 78, 45 ] */
+console.log(element);
+/* Output: 98 */
 
 var friends = ['Balam', 'Kalam', 'Salam'];
 friends.push('Halim');
 friends.push('Hasan');
-// console.log(friends);
+console.log(friends);
+/* Output: 
+[ 'Balam', 'Kalam', 'Salam', 'Halim', 'Hasan' ] */
 
-// Use pop to get rid of the last element.
-console.log(numbers);
-numbers.pop();
-// numbers.pop();
-var element = numbers.pop();
-console.log(numbers);
-console.log(element);
-
-// The unshift() method adds new elements to the beginning of an array.
-// The unshift() method overwrites the original array.
+/* The unshift() method adds new elements to the beginning of an array.
+The unshift() method overwrites the original array. */
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
-// console.log(fruits);
+console.log(fruits);
+/* Output: 
+[ 'Banana', 'Orange', 'Apple', 'Mango' ] */
 fruits.unshift("Lemon","Pineapple");
-// console.log(fruits);
+console.log(fruits);
+/* Output: 
+[ 'Lemon', 'Pineapple', 'Banana', 'Orange', 'Apple', 'Mango' ] */
 
-// Shift (remove) the first element of the array:
+/* Shift (remove) the first element of the array: */
 var fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.shift();
 console.log(fruits);
+/* Output: 
+[ 'Orange', 'Apple', 'Mango' ] */
 
 
