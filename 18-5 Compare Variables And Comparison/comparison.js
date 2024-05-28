c31354fc469654f5cd5527154daa4d7fd89e9bfc
shihab20105 <shihabur10@gmail.com>
2024-05-28 16:35:32 +0100

    Discovered Compare Variables And Comparison Operator

--- 18-5 Compare Variables And Comparison/comparison.js
+++ 18-5 Compare Variables And Comparison/comparison.js
@@ -1,30 +1,68 @@
-// console.log(5 < 6);
-// console.log(5 > 6);
-// console.log(5 == 6);
-// console.log(6 == 6);
-// console.log(5 != 6);
-// console.log(15 != 15);
+/*                          Comparison */
 
-// Less than or equals to 
-// console.log(5 <= 6);
-// console.log(6 <= 6);
-// console.log(16 <= 6);
+console.log(5 < 6);
+/* Output: 
+true */
+console.log(5 > 6);
+/* Output:
+false */
+console.log(5 == 6);
+/* Output:
+false */
+console.log(6 == 6);
+/* Output:
+true */
+console.log(5 != 6);
+/* Output:
+true */
+console.log(15 != 15);
+/* Output:
+false */
 
-// Greater than or equals to
-// console.log(5 >= 6);
-// console.log(5 >= 2);
-// console.log(2 >= 2);
+/* Less than or equals to */ 
+console.log(5 <= 6);
+/* Output:
+true */
+console.log(6 <= 6);
+/* Output:
+true */
+console.log(16 <= 6);
+/* Output:
+false */
+
+/* Greater than or equals to */
+console.log(5 >= 6);
+/* Output:
+false */
+console.log(5 >= 2);
+/* Output:
+true */
+console.log(2 >= 2);
+/* Output:
+true */
 
 var num1 = 56;
 var num2 = 89;
 console.log(num1 < num2);
+/* Output:
+true */
 console.log(num1 > num2);
+/* Output:
+false */
 console.log(num1 === num2);
+/* Output:
+false */
 console.log(num1 != num2);
+/* Output:
+true */
 console.log(num1 >= num2);
+/* Output:
+false */
 console.log(num1 <= num2);
+/* Output:
+true */
 
-// Multiple conditions
-// money1 > money2 && result1 > result2 && height1 >height2
-// money1 > money2 || result1 > result2 || height1 > height2
+/* Multiple conditions
+money1 > money2 && result1 > result2 && height1 >height2
+money1 > money2 || result1 > result2 || height1 > height2 */
 
