e98735a278da05b069b54fd5e408b0c88bff53b4
shihab20105 <shihabur10@gmail.com>
2024-05-29 22:28:13 +0100

    Expolred Handle Multiple Conditions, And Or

--- 18-7 Handle Multiple Conditions, And Or/multiple-conditions.js
+++ 18-7 Handle Multiple Conditions, And Or/multiple-conditions.js
@@ -1,30 +1,75 @@
-// var isGraduated = true;
+/*                          Multiple Conditions */
+
+
+/* var isGraduated = true;
 // var salary = 25000;
-// var salary = 75000;
+var salary = 75000;
 // var cars = 0;
-// var cars = 1;
+var cars = 1;
+ */
+
+/* if(isGraduated == true){
+    console.log('lets make a deal');
+} */
+/* Output:
+lets make a deal */
 
 
-// if(isGraduated === true){
-//     console.log('lets make a deal');
-// }
-// else{
-//     console.log('you can not able to get it');
-// }
 
-// if(isGraduated == true && salary > 50000 ){
-//     console.log('lets make a deal');
-// }
-// else{
-//     console.log('you can not able to get it');
-// }
+/* if(isGraduated == true){
+    console.log('lets make a deal');
+}
+else{
+    console.log('you can not able to get it');
+} */
+/* Output:
+lets make a deal */
 
-// if(isGraduated == true && salary > 50000 && cars >= 1){
-//     console.log('lets make a deal');
-// }
-// else{
-//     console.log('you can not able to get it');
-// }
+
+
+/* if(isGraduated == true && salary > 50000 ){
+    console.log('lets make a deal');
+}
+else{
+    console.log('you can not able to get it');
+} */
+/* Output:
+you can not able to get it */
+
+
+
+/* if(isGraduated == true && salary > 50000 ){
+    console.log('lets make a deal');
+}
+else{
+    console.log('you can not able to get it');
+} */
+/* Output:
+lets make a deal */
+
+
+
+/* if(isGraduated == true && salary > 50000 && cars >= 1){
+    console.log('lets make a deal');
+}
+else{
+    console.log('you can not able to get it');
+} */
+/* Output:
+you can not able to get it */
+
+
+
+/* if(isGraduated == true && salary > 50000 && cars >= 1){
+    console.log('lets make a deal');
+}
+else{
+    console.log('you can not able to get it');
+} */
+/* Output:
+lets make a deal */
+
+
 
 // var isGraduated = true;
 var isGraduated = false;
@@ -35,16 +80,68 @@ var salary = 61000;
 // var cars = 1;
 var cars = 0;
 
-if(isGraduated === true || salary > 50000 || cars >= 1){
+/* if(isGraduated === true || salary > 50000){
+    console.log('let us work together');
+}
+else{
+    console.log('i will contact to other person');
+} */
+/* Output: 
+i will contact to other person*/
+
+
+
+/* if(isGraduated === true || salary > 50000){
+    console.log('let us work together');
+}
+else{
+    console.log('i will contact to other person');
+} */
+/* Output: 
+let us work together*/
+
+
+
+/* if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('let us work together');
+}
+else{
+    console.log('i will contact to other person');
+} */
+/* Output: 
+let us work together*/
+
+
+
+/* if(isGraduated === true || salary > 50000 || cars >= 1){
+    console.log('let us work together');
+}
+else{
+    console.log('i will contact to other person');
+} */
+/* Output: 
+let us work together*/
+
+
+
+
+/* if((isGraduated === true && salary > 50000) || cars >= 1){
+    console.log('let us work together');
+}
+else{
+    console.log('i will contact to other person');
+} */
+/* Output: 
+i will contact to other person*/
+
+
+
+
+if(isGraduated === true && (salary > 50000 || cars >= 1)){
     console.log('let us work together');
 }
 else{
     console.log('i will contact to other person');
 }
-
-// if((isGraduated === true && salary > 50000) || cars >= 1){
-
-// }
-if(isGraduated === true && (salary > 50000 || cars >= 1)){
-
-}
+/* Output: 
+i will contact to other person*/
